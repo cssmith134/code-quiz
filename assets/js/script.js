b6edb4f412191f7096809cbcd2a155cefc9648c5
REX <cssmith431@yahoo.com>
2022-01-13 14:02:18 -0500

    add quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+var quizBodyEl = document.getElementById("quiz");
+var startQuizBtnEl = document.getElementById("#startBtn");
+
+
+
+
+
+
+
+var quizQuestions = [{
+    question: "How many elements can you apply an 'ID' attribute to?",
+    choiceA: "As many as you want",
+    choiceB: "3",
+    choiceC: "1",
+    choiceD: "128",
+    correctAnswer: "c"},
+  {
+    question: "What does API stand for?",
+    choiceA: "Application Programming Info",
+    choiceB: "Application Programming Interface",
+    choiceC: "Application Process Interval",
+    choiceD: "Application Process Info",
+    correctAnswer: "b"},
+   {
+    question: "What is used primarily to add structure to a web page?",
+    choiceA: "HTML",
+    choiceB: "CSS",
+    choiceC: "Python",
+    choiceD: "React.js",
+    correctAnswer: "a"},
+    {
+    question: "What selector is used to target class in CSS",
+    choiceA: "#",
+    choiceB: "$",
+    choiceC: ".",
+    choiceD: "&",
+    correctAnswer: "c"},
+    {
+    question: "How do you create a variable in Javascript?",
+    choiceA: "var",
+    choiceB: "$",
+    choiceC: "variable",
+    choiceD: "Go",
+    correctAnswer: "a"},  
+    {
+    question: "What command pushs commit to main branch in terminal?",
+    choiceA: "git push main",
+    choiceB: "git push to main",
+    choiceC: "git push origin main",
+    choiceD: "git add main",
+    correctAnswer: "c"},
+    {
+    question: "What does Math.floor do?",
+    choiceA: "Erases a math equation",
+    choiceB: "Rounds down a decimal number ",
+    choiceC: "Creates a random number",
+    choiceD: "None of the above",
+    correctAnswer: "b"},
+];
