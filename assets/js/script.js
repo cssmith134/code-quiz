413c936101d8d595e097daf99db57e2c7b2e7bfb
REX <cssmith431@yahoo.com>
2022-01-13 23:19:01 -0500

    first round questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,69 @@
 var quizBodyEl = document.getElementById("quiz");
-var startQuizBtnEl = document.getElementById("#startBtn");
+var quizChoice1El = document.getElementById("choice1")
+var quizChoice1El = document.getElementById("choice2")
+var quizChoice1El = document.getElementById("choice3")
+var quizChoice1El = document.getElementById("choice4")
+var startQuizBtnEl = document.getElementById("startBtn");
+var quizAnswerBtnEl = document.querySelector(".btn");
 
 
 
 
+function startGame (event) {
+  $("#start-page").remove();
+  var questionTextEl = document.createElement("div");
+  var question = quizQuestions[0].question
+  questionTextEl.textContent = question
+  
 
+  quizBodyEl.appendChild(questionTextEl);
+  
+  var choiceButtonEl = document.createElement("button");
+  choiceButtonEl.type = "button";
+  choiceButtonEl.innerHTML = quizQuestions[0].choiceA;
+  choiceButtonEl.className = "btn1 btn btn-primary m-2";
+
+  quizBodyEl.appendChild(choiceButtonEl);
+
+  var choiceButtonEl = document.createElement("button");
+  choiceButtonEl.type = "button";
+  choiceButtonEl.innerHTML = quizQuestions[0].choiceB;
+  choiceButtonEl.className = "btn2 btn btn-primary m-2";
+
+  quizBodyEl.appendChild(choiceButtonEl);
+
+  var choiceButtonEl = document.createElement("button");
+  choiceButtonEl.type = "button";
+  choiceButtonEl.innerHTML = quizQuestions[0].choiceC;
+  choiceButtonEl.className = "btn3 btn btn-primary m-2";
+
+  quizBodyEl.appendChild(choiceButtonEl);
+
+  var choiceButtonEl = document.createElement("button");
+  choiceButtonEl.type = "button";
+  choiceButtonEl.innerHTML = quizQuestions[0].choiceD;
+  choiceButtonEl.className = "btn4 btn btn-primary m-2";
+
+  quizBodyEl.appendChild(choiceButtonEl);
+  $(".btn1").on("click", function() { alert("incorrect")});
+  $(".btn3").on("click", function() { alert("correct")});
+  $(".btn2").on("click", function() { alert("incorrect")});
+  $(".btn4").on("click", function() { alert("incorrect")});
+  
+
+};
+
+startQuizBtnEl.addEventListener("click", startGame);
+
+  function nextQuestion (event) {
+      $("#quiz").remove();
+  };
+  
+
+ 
+
+
+ 
 
 
 var quizQuestions = [{
@@ -13,47 +72,47 @@ var quizQuestions = [{
     choiceB: "3",
     choiceC: "1",
     choiceD: "128",
-    correctAnswer: "c"},
+    correctAnswer: "1"},
   {
     question: "What does API stand for?",
     choiceA: "Application Programming Info",
     choiceB: "Application Programming Interface",
     choiceC: "Application Process Interval",
     choiceD: "Application Process Info",
-    correctAnswer: "b"},
+    correctAnswer: "Application Programming Interface"},
    {
     question: "What is used primarily to add structure to a web page?",
     choiceA: "HTML",
     choiceB: "CSS",
     choiceC: "Python",
     choiceD: "React.js",
-    correctAnswer: "a"},
+    correctAnswer: "HTML"},
     {
     question: "What selector is used to target class in CSS",
     choiceA: "#",
     choiceB: "$",
     choiceC: ".",
     choiceD: "&",
-    correctAnswer: "c"},
+    correctAnswer: "."},
     {
     question: "How do you create a variable in Javascript?",
     choiceA: "var",
     choiceB: "$",
     choiceC: "variable",
     choiceD: "Go",
-    correctAnswer: "a"},  
+    correctAnswer: "var"},  
     {
     question: "What command pushs commit to main branch in terminal?",
     choiceA: "git push main",
     choiceB: "git push to main",
     choiceC: "git push origin main",
     choiceD: "git add main",
-    correctAnswer: "c"},
+    correctAnswer: "git push origin main"},
     {
     question: "What does Math.floor do?",
     choiceA: "Erases a math equation",
-    choiceB: "Rounds down a decimal number ",
+    choiceB: "Rounds down a decimal number",
     choiceC: "Creates a random number",
     choiceD: "None of the above",
-    correctAnswer: "b"},
+    correctAnswer: "Rounds down a decimal number"},
 ];
